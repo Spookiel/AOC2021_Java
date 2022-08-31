750f5b66977e93cfd563642658fde800f2ee067e
Luke M <luke.moran2004@gmail.com>
2022-08-31 12:07:18 +0100

    Finished Day 4

--- src/Day4.java
+++ src/Day4.java
@@ -14,20 +14,38 @@ public class Day4 {
 
         int[][] nums = new int[5][5];
         int[][] marked = new int[5][5];
-
+        boolean won = false;
 
         public Board(String [] init){
 
-            String[] l = init[0].split(" ");
+
+            System.out.println(Arrays.toString(init) + " IN CLASS");
+            //String[] l = init[0].split(" ");
+            //System.out.println(Arrays.toString(init[1].split(" ")));
             //System.out.println(Arrays.deepToString(l));
-            for(int i=0; i < 5; i++) for(int j=0; j < 5; j++){
-                if(!Objects.equals(l[j], "") && !Objects.equals(l[j], " ")){
-                    this.nums[i][j] = Integer.parseInt(l[j]);
+            for(int i=0; i < 5; i++){
+                String[] spl = init[i].replace("  ", " ").split(" ");
+                System.out.println(Arrays.toString(spl));
 
+
+                System.out.println(Arrays.toString(spl));
+                int read = 0;
+                int cur = 0;
+                while(read < 5){
+
+                    if(spl[cur].equals("")){
+                        cur++;
+                        continue;
+                    }
+
+
+                    this.nums[i][read] = Integer.parseInt(spl[cur]);
+
+
+
+                    read++;
+                    cur++;
                 }
-
-
-
 
             }
 
@@ -36,6 +54,8 @@ public class Day4 {
 
 
         }
+
+        public Board(){}
 
         public boolean check_rows(){
 
@@ -47,24 +67,24 @@ public class Day4 {
 
                 }
 
-                if(!ok) return false;
+                if(ok) return true;
 
 
             }
 
-            return true;
+            return false;
         }
 
         public boolean check_cols(){
             for(int i=0; i < 5; i++){
                 boolean ok = true;
                 for(int j=0; j < 5; j++){
-                    if(marked[j][i] == 0) ok = false;
+                    if(this.marked[j][i] == 0) ok = false;
 
                 }
-                if(!ok) return false;
+                if(ok) return true;
             }
-            return true;
+            return false;
 
         }
 
@@ -75,15 +95,15 @@ public class Day4 {
 
             boolean ok = true;
             for(int i=0; i < 5; i++){
-                if(marked[i][i] == 0) ok = false;
+                if(this.marked[i][i] == 0) ok = false;
 
             }
 
-            if(!ok) return false;
+            if(ok) return true;
 
             boolean ok1 = true;
             for(int i=0; i < 5; i++){
-                if(marked[4-i][i] == 0) ok1 = false;
+                if(this.marked[4-i][i] == 0) ok1 = false;
 
             }
             return ok1;
@@ -92,7 +112,7 @@ public class Day4 {
 
         public boolean check_won(){
 
-            return this.check_cols() || this.check_rows() || this.check_diags();
+            return this.check_cols() || this.check_rows();// || this.check_diags();
 
 
         }
@@ -102,7 +122,7 @@ public class Day4 {
             for(int i=0; i < 5; i++){
                 for(int j=0; j < 5; j++){
 
-                    if(this.nums[i][j] == num) marked[i][j] = 1;
+                    if(this.nums[i][j] == num) this.marked[i][j] = 1;
 
                 }
 
@@ -114,6 +134,17 @@ public class Day4 {
         }
 
 
+        public int sum_unmarked(){
+            int tsum = 0;
+            for(int i=0; i < 5; i++) for(int j=0; j < 5; j++){
+                if(this.marked[i][j] == 0) tsum += this.nums[i][j];
+
+
+            }
+            return tsum;
+        }
+
+
     }
 
     public void parseBoard(String[] lines){
@@ -121,8 +152,9 @@ public class Day4 {
         this.boards.add(new Board(lines));
 
     }
-    public void run1() throws IOException{
 
+
+    public void parse() throws IOException{
         BufferedReader br = new BufferedReader(new FileReader("Day4.in"));
 
 
@@ -138,10 +170,9 @@ public class Day4 {
 
         String curline;
         while((curline = br.readLine())!= null){
-            br.readLine();
             String[] inp = new String[5];
             for(int i=0; i < 5; i++) inp[i] = br.readLine();
-
+            //System.out.println(Arrays.toString(inp)+" HERE");
             this.parseBoard(inp);
 
 
@@ -151,10 +182,77 @@ public class Day4 {
 
     }
 
+    public void run1() throws IOException{
+
+
+
+        Board winner = new Board();
+
+        int wnum = 1;
+        boolean won = false;
+        for(int to_play: this.order){
+            for(Board b: this.boards){
+                b.play_num(to_play);
+                if(b.check_won()){
+                    winner = b;
+                    won = true;
+                    wnum = to_play;
+                    break;
+                }
+
+
+
+            }
+            if(won) break;
+
+
+        }
+
+        System.out.println(won);
+        System.out.println(Arrays.deepToString(winner.marked));
+        System.out.println(Arrays.deepToString(winner.nums));
+        System.out.println(winner.sum_unmarked()*wnum);
+
+
+
+
+    }
+
+    public void run2(){
+        Board winner = new Board();
+        int wnum = 1;
+        boolean won = false;
+        for(int to_play: this.order){
+            for(Board b: this.boards){
+                if(!b.won){
+                    b.play_num(to_play);
+                    if(b.check_won()){
+                        b.won = true;
+                        winner = b;
+                        wnum = to_play;
+                }
+
+                }
+
+
+
+            }
+
+
+        }
+
+
+        System.out.println(winner.sum_unmarked()*wnum);
+
+
+
+    }
+
     public static void main(String[] args) throws IOException{
         Day4 day = new Day4();
-
+        day.parse();
         day.run1();
+        day.run2();
 
     }
 }
